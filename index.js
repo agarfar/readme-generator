cc08ba02c1557fb3f142392430518e1f50710a4a
Soprano <antfar67@gmail.com>
2022-11-27 17:39:17 -0800

    Outlined README generator functions, saving edits before installing inquirer

---
+++ index.js
@@ -0,0 +1,99 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js")
+
+// inquirer.prompt(questions).then((answers) => {
+//   const template = `
+//         <div>
+//             <p>My favorite color is ${answers.color}</p>
+//             <p>Sushi like status: ${answers.sushi}</p>
+//             <p>Fancy pasta: ${answers.pasta}</p>
+//         </div>
+//     `;
+
+//   fs.writeFile("./output.html", template, () => {
+//     console.log("pizza is out the oven");
+//   });
+// });
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "user",
+    },
+
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "user",
+    },
+
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description",
+    },
+
+    {
+        type: "input",
+        message: "Please list your installation instructions: ",
+        name: "install",
+    },
+
+    {
+        type: "input",
+        message: "Please list the application's usage instructions: ",
+        name: "usage",
+    },
+
+    {
+        type: "input",
+        message: "Please list the application's test instructions: ",
+        name: "tests",
+    },
+
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo? ",
+        name: "contributing",
+    },
+
+    {
+        type: "list",
+        message: "What license do you need?",
+        name: "license",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+
+];
+
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const readMeContent = generateMarkdown.generateMarkdown(answers)
+        fs.writeFile('README.md', readMeContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README!')
+        );
+    })
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+
+// }
+
+
+
+// TODO: Create a function to initialize app
+// function init() { }
+
+// // Function call to initialize app
+// init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,83 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  else {
+    return '';
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {
+
+// }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  else {
+    return `This project is covered under the ${license} license.`;
+  }
+}
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseLink(data.license)
+  const licenseSection = renderLicenseSection(data.license)
+
+  return `#${data.title}
+  ${licenseBadge}
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.install}
+
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  ${licenseSection}
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  Github Profile: https://github.com/${data.user}
+
+  Please address all questions regarding this project to the following email: ${email}
+
+  `;
+}
+
+module.exports = {
+  generateMarkdown,
+  renderLicenseBadge,
+  // renderLicenseLink,
+  renderLicenseSection
+};
